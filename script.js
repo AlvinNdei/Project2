f9b4001bb32a93a48fcb568ec66302de9219f943
AlvinNdei <alvinmuriuki714@gmail.com>
2026-06-14 16:46:46 +0300

    Added an event listener

---
+++ script.js
@@ -0,0 +1,6 @@
+let submit=document.getElementById("submit-btn");
+
+submit.addEventListener("click",function(){
+    // alert("Your information has been submitted")
+    console.log("button clicked successfully");
+});
