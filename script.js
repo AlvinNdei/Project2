6fff4ca334ce08d8296b768e51f6e9dcae26dd99
AlvinNdei <alvinmuriuki714@gmail.com>
2026-06-14 17:01:43 +0300

    Event listner

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let submit=document.getElementById("submit-btn");
+let contact=document.getElementById("contact-btn");
 
 submit.addEventListener("click",function(){
     // alert("Your information has been submitted")
     console.log("button clicked successfully");
 });
+contact.addEventListener("click", function(){
+    console.log("contact button")
+})
