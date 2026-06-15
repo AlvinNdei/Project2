c2e6a385e7193d619a7c36a0442971239bb3ee15
AlvinNdei <alvinmuriuki714@gmail.com>
2026-06-15 17:10:20 +0300

    exception handling

--- Objects.js
+++ Objects.js
@@ -195,3 +195,15 @@ class Vec{
 const vec = new Vec(3,2);
 vec.plus();
 vec.minus();
+//exceptions
+let a=0;
+let b=4;
+function division(){
+    let div=a/b;
+    
+}
+try{
+    console.log("The division between an and b is:" + division());
+}catch(error){
+    console.log("An error was encountered: " + error);
+}
